ec2e5f9f66d233efe288818a5e72a2ac4abcb877
Mckenzie <mckenzieturner@protonmail.com>
2022-08-18 21:07:49 -0300

    You2Me
    Python console based Youtube downloader! Can download load individual links or search for Titles to download

---
+++ main.py
@@ -0,0 +1,129 @@
+'''
+Author: Mckenzie Turner
+Date Created: Aug 16, 2022
+Name: You2Me-C
+'''
+import os.path
+
+import pytube.extract
+from pytube import YouTube, Search
+import menu3
+
+media_dir = os.path.expanduser('~/Desktop/You2Me')
+audio_dir = os.path.expanduser(media_dir + '/Audio')
+video_dir = os.path.expanduser(media_dir + '/Video')
+
+
+def start():
+    active_dir = os.path.isdir(media_dir)
+
+    while active_dir:
+        print(f'~~~[LOG]~~~  Directory {media_dir} located')
+        search()
+        break
+    else:
+        print(f'~~~[LOG]~~~  Directory {media_dir} not located')
+        print(f'~~~[LOG]~~~  Directories:\n  -{media_dir}\n  -{audio_dir}\n  -{video_dir}\nHave now been created')
+        os.mkdir(media_dir)
+        os.mkdir(audio_dir)
+        os.mkdir(video_dir)
+        search()
+
+
+def search():
+    '''
+    Master lists for use through "search()"
+    '''
+    sel_options = ['Download Audio Files', 'Download Video Files']
+    type_options = ['Search for video?', 'Download a specific video?']
+    title_url = {}
+
+    core_search_menu = menu3.Menu(True)
+    selection_menu = core_search_menu.menu('Please select one:', sel_options, "Press 'q' to quit now")
+
+    if sel_options[selection_menu - 1] == sel_options[0]:
+        ''' if Audio is selected '''
+        print(f'~~~[LOG]~~~ {sel_options[selection_menu - 1]} selected')
+
+        audi_sel_menu = core_search_menu.menu('Please select one:', type_options)
+
+        if type_options[audi_sel_menu - 1] == type_options[0]:
+            print(f'~~~[LOG]~~~ {sel_options[selection_menu - 1]} selected')
+
+            youtube_search = Search(input("What would you like to search for: "))
+            for res in youtube_search.results:
+                print(f'~~~[LOG]~~~ adding {res.title} + {res.watch_url} to title_url --dict-- ')
+                title_url[res.title] = res.watch_url
+
+            audio_titles = [t for t in title_url.keys()]
+            audio_title_menu = core_search_menu.menu('Please select one', audio_titles)
+            if audio_titles[audio_title_menu - 1]:
+                print(f'~~~[LOG]~~~ Downloading {audio_titles[audio_title_menu - 1]} to {audio_dir}')
+                audio_download(audio_dir, title_url.get(audio_titles[audio_title_menu - 1]))
+
+        elif type_options[audi_sel_menu - 1] == type_options[1]:
+            ''' if Specific video is selected '''
+            print(f'~~~[LOG]~~~ {sel_options[selection_menu - 1]} selected')
+
+            try:
+                audio_link = input('Please paste link to video here:')
+                if 'https://' not in audio_link:
+                    print(f"~~~[LOG]~~~ 'https://' not found in {audio_link}")
+                else:
+                    print(f"~~~[LOG]~~~ 'https://' found in {audio_link}")
+                    print(f"~~~[LOG]~~~ Downloading now..........")
+                    audio_download(audio_dir, audio_link)
+            except EOFError as err:
+                print(err)
+
+
+    elif sel_options[selection_menu - 1] == sel_options[1]:
+        ''' if Video is selected '''
+        print(f'~~~[LOG]~~~ {sel_options[selection_menu - 1]} selected')
+        vs_core_menu = menu3.Menu(True)
+        vs_menu = vs_core_menu.menu('Please select an option now', type_options)
+
+        if type_options[vs_menu - 1] == type_options[0]:
+            print(f'~~~[LOG]~~~ {type_options[vs_menu - 1]} selected')
+            video_search = Search(input("What would you like to search for: ")).results
+
+            for res in video_search:
+                print(f'~~~[LOG]~~~ adding {res.title} + {res.watch_url} to title_url --dict-- ')
+                title_url[res.title] = res.watch_url
+
+            video_search_results = [t for t in title_url.keys()]
+            vs_title_menu = vs_core_menu.menu("Please select one to download: ", video_search_results)
+            if video_search_results[vs_title_menu - 1]:
+                print(f'~~~[LOG]~~~ Downloading {video_search_results[vs_title_menu - 1]} '
+                      f'to {video_dir}')
+                video_download(video_dir, title_url.get((video_search_results[vs_title_menu - 1])))
+
+
+        elif type_options[vs_menu - 1] == type_options[1]:
+            print(f'~~~[LOG]~~~ {type_options[vs_menu - 1]} selected')
+            video_download(video_dir, input("Please paste the link here: "))
+            try:
+                link = input('Please paste link to video here:')
+                if 'https://' not in link:
+                    print(f"~~~[LOG]~~~ 'https://' not found in {link}")
+                else:
+                    print(f"~~~[LOG]~~~ 'https://' found in {link}")
+                    print(f"~~~[LOG]~~~ Downloading now..........")
+                    audio_download(audio_dir, link)
+            except EOFError as err:
+                print(err)
+
+
+def video_download(path, link):
+    print(f'~~~[LOG]~~~ Downloading to path: \n {path}')
+    YouTube(link).streams.get_highest_resolution().download(path)
+
+
+def audio_download(path, link):
+    print(f'~~~[LOG]~~~ Downloading to:\n  {path}')
+    YouTube(link).streams.get_audio_only().download(path)
+
+
+
+
+start()
